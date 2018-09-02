6577c9e3999bad093326c13e4bfd2c51884d1e1f
Whitney Seiler <whitney.seiler@gmail.com>
2018-09-02 12:29:18 -0700

    build basic ReactJS file structure

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div className="main">
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const request = require('request');
+
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(3000, () => {
+  console.log('now listening on port 3000');
+});
